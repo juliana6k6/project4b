b779b122303eab9fde09bfa654e83f22813697ad
Juliana <juliana6k@mail.ru>
2024-05-09 01:41:53 +0300

    задание 3

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,7 +4,22 @@ from materials.models import Course, Lesson
 from rest_framework.fields import SerializerMethodField
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    lesson = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    count_lessons_in_course = SerializerMethodField()
+
+    @staticmethod
+    def get_count_lessons_in_course(course):
+        return Lesson.objects.filter(course=course).count()
+
+    # def get_lesson_count(self, instance):
+    #     return instance.lesson_set.count()
 
     # 2-ой способ
     # lesson_count = serializers.SerializerMethodField()
@@ -19,14 +34,10 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
-
 
 class CourseDetailSerializer(serializers.ModelSerializer):
     count_lessons_in_course = SerializerMethodField()
+    lesson = LessonSerializer(source='lesson_set', many=True, read_only=True)
 
     @staticmethod
     def get_count_lessons_in_course(course):
